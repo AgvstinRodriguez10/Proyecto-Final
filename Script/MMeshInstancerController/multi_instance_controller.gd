3614afcb932895d4f921f43435d742deffeaf9e1
NicolasSaucedoDev <nicolas.saucedo.dev@gmail.com>
2026-02-24 08:46:12 -0300

    agregando chekcer para actualizador de multimesh en editor

--- Script/MMeshInstancerController/multi_instance_controller.gd
+++ Script/MMeshInstancerController/multi_instance_controller.gd
@@ -4,22 +4,32 @@ extends MultiMeshInstance3D
 ## Setear el padre de los markers a usar, recordar reiniciar la escena
 @export var markers_parent: Node3D
 
+@export var refresh_editor:bool = false
+
+var _last_count := -1
+
 func _ready():
 	setup_multimesh()
 
 func _process(delta: float) -> void:
-	if Engine.is_editor_hint():
+	# Esto es para que no se calcule el procces en ejecucion del juego.
+	# solo sigue con su ejecucion si esta en el editor nomas
+	if not Engine.is_editor_hint():
+		return
+
+	if refresh_editor:
 		setup_multimesh()
 
 func setup_multimesh():
-	if !markers_parent:
+	# este codigo se encarga de crear los faroles de cada marker
+	if !markers_parent or not multimesh:
 		return
+
 	var markers := markers_parent.get_children()
-	
-	if not multimesh:
-		return
-	
+
+	_last_count = markers.size()
+
 	multimesh.instance_count = markers.size()
 
-	for i in markers.size():
+	for i in range(markers.size()):
 		multimesh.set_instance_transform(i, markers[i].global_transform)
