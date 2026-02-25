3514a43a18a055149f1dd0cc06fccde0820a1b1e
NicolasSaucedoDev <nicolas.saucedo.dev@gmail.com>
2026-02-25 13:28:54 -0300

    acomodando main, collisiones de escalera, incluyendo yagui, y adaptando codigo

--- Script/Characters/Yagui/yagui.gd
+++ Script/Characters/Yagui/yagui.gd
@@ -4,14 +4,8 @@ class_name Yagui
 func _ready() -> void:
 	super._ready()
 	animationPlayer = $Yagui1/AnimationPlayer
-	actualizarSpeed(1)
-	velocity_forward = 400
+	#actualizarSpeed(1)
+	velocity_forward = 20.05
 
-#func _physics_process(delta: float) -> void:
-	#if !is_movie:
-		#currentState = STATES.RUN
-	
-	#animationController(delta)
-
-func actualizarSpeed(speed:float):
-	animationPlayer.speed_scale = speed * (current_velocity_forward * .005)
+#func actualizarSpeed(speed:float):
+	#animationPlayer.speed_scale = speed * (current_velocity_forward * .005)
